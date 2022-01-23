3d4e208530c3d2ffc7c960595ed29d2f8cda5619
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-01-23 22:39:29 +0300

    new pages

--- components/About.tsx
+++ components/About.tsx
@@ -4,8 +4,8 @@ const About = () => {
   return (
     <section className="container text-gray-400 px-5 py-36 flex flex-col content-center animate-fade   ">
       <div className=" mx-auto flex flex-col text-center  ">
-        <h1 className="sm:text-4xl text-3xl mb-4 font-bold text-gray-900">
-          Hakan Gundogdu
+        <h1 className="sm:text-2xl text-2xl mb-4 font-bold text-gray-900">
+          About Me{' '}
         </h1>
         <p className=" text-xl leading-relaxed">
           Frontend Developer based in{' '}
