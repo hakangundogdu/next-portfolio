54f105f2fb97e27987db18de904f13b29092605b
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-02-21 13:22:34 +0300

    projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,10 +7,57 @@ const Projects = () => {
       className="container px-4 sm:px-12 py-16   animate-fade    "
     >
       <h1 className="text-2xl mb-6 font-bold text-slate-900">Projects</h1>
+      <div className="container  ">
+        <div className=" flex border-slate-200 overflow-hidden border rounded-xl ">
+          <div className="w-1/2   ">
+            <img
+              src="/images/project-1.png"
+              alt="project"
+              className=" w-full object-contain   "
+            />{' '}
+          </div>
 
-      <div className="container grid md:grid-cols-2 grid-cols-1 gap-4 ">
-        <div className=" pt-8  rounded-xl bg-gradient-to-br from-teal-500 to-cyan-800  px-8 shadow-md aspect-square">
-          <p className=" text-xl font-semibold  text-slate-100 "> Project 1 </p>
+          <div className="w-1/2 bg-gradient-to-br from-teal-500 to-cyan-800 flex flex-col justify-between px-8 py-8">
+            <div>
+              <p className=" text-xl  mb-4 font-semibold  text-white">
+                {' '}
+                EasyBank Landing Page{' '}
+              </p>
+              <p className="  mb-4  text-teal-100"> EasyBank Landing Page </p>
+              <p className="  mb-4  text-teal-100"> React / NextJS</p>
+            </div>
+
+            <div className="flex space-x-4  text-sm font-medium">
+              <div className="flex-auto flex space-x-4">
+                <button
+                  className="h-10 px-6 font-semibold rounded-md bg-slate-900 text-white"
+                  type="submit"
+                >
+                  Live
+                </button>
+                <button
+                  className="h-10 px-6 font-semibold rounded-md border border-slate-300 bg-teal-100 text-teal-500"
+                  type="button"
+                >
+                  Code
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="container grid md:grid-cols-2 grid-cols-1 gap-4  my-4">
+        <div className="relative flex flex-col  rounded-xl bg-gradient-to-br from-teal-500 to-cyan-800   shadow-md aspect-square">
+          <p className=" text-xl px-8 pt-6 mb-4 font-semibold  text-slate-100">
+            {' '}
+            EasyBank Landing Page{' '}
+          </p>
+          <img
+            src="/images/project-1.png"
+            alt="project"
+            className="absolute bottom-0 w-full object-contain   "
+          />
         </div>
         <div className="  pt-8  rounded-xl bg-gradient-to-bl from-blue-400 via-blue-600 to-blue-900  px-8 aspect-square shadow-md">
           <p className=" text-xl font-semibold  text-slate-100"> Project 2 </p>
@@ -21,7 +68,7 @@ const Projects = () => {
         <div className="   pt-8  rounded-xl bg-gradient-to-tr from-rose-700 to-pink-600  px-8 aspect-square shadow-md">
           <p className=" text-xl font-semibold  text-slate-100 "> Project 4 </p>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
