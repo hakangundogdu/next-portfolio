1b0a2ff244b1b7dfd571c46bea4b35dbbe6f4600
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-02-21 18:50:22 +0300

    cv

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const cv = () => {
   return (
-    <div className=" mx-auto flex items-center justify-center h-screen  ">
+    <div className=" mx-auto flex items-center justify-center md:h-screen  ">
       <embed height="100%" width="100%" src="/HakanGundogdu.pdf"></embed>
     </div>
   );
