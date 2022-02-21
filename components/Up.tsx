5b446f4c82f61ef121518bfeecc3330bb598ffd2
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-02-21 23:45:39 +0300

    scrolltop

--- components/Up.tsx
+++ components/Up.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { FaCaretUp } from 'react-icons/fa';
 
 const Up = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
   return (
-    <a
-      className="hidden sm:block fixed right-10 bottom-10 rounded-xl items-center bg-slate-100 text-slate-500 z-30 py-3 px-3 shadow-lg"
-      href="#top"
-    >
-      <FaCaretUp />
-    </a>
+    <>
+      {isVisible && (
+        <button
+          className="hidden  sm:block fixed right-10 bottom-10 rounded-xl items-center bg-slate-100 text-slate-500 z-30 py-3 px-3 shadow-lg"
+          onClick={scrollToTop}
+        >
+          <FaCaretUp />
+        </button>
+      )}
+    </>
   );
 };
 
