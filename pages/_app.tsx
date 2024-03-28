9f56cc5a0f2a06a30d4e23ecd41a99510dd07abf
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2024-03-28 15:10:11 +0300

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.css";
 import Nav from "../components/Nav";
 import type { AppProps } from "next/app";
@@ -11,7 +10,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </div>
       <Analytics />
-      <SpeedInsights />
     </>
   );
 }
